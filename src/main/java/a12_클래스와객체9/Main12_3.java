2c4135b1f2378f021099db02afa7d19605a8d83c
jinsu291 <wlstn291@naver.com>
2022-07-23 05:15:16 +0900

    Main12_3 추가

---
+++ src/main/java/a12_클래스와객체9/Main12_3.java
@@ -0,0 +1,24 @@
+package a12_클래스와객체9;
+
+public class Main12_3 {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+        a사람.달리기_속력 = 20;
+        a사람.달리다();
+        // 사람이 20km/h로 달립니다.
+        a사람.달리다();
+        // 사람이 20km/h로 달립니다.
+
+        a사람.달리기_속력 = 50;
+        a사람.달리다();
+        // 사람이 50km/h로 달립니다.
+    }
+
+    static class 사람 {
+        int 달리기_속력;
+
+        void 달리다() {
+            System.out.println("사람이 " + 달리기_속력 + "km/h로 달립니다.");
+        }
+    }
+}
