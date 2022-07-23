6d13b702e41ff84513bffc1445ccc5fea05e2b0b
jinsu291 <wlstn291@naver.com>
2022-07-24 00:56:14 +0900

    Main14_7 추가

---
+++ src/main/java/a14_생성자1/Main14_7.java
@@ -0,0 +1,42 @@
+package a14_생성자1;
+
+// 문제 : 아래와 같이 출력 되도록 해주세요.
+// 조건 : 생성자를 오직 3개만 사용해서 만들어주세요.
+
+public class Main14_7 {
+    public static void main(String[] args) {
+        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
+
+        System.out.println(엄지손가락_길이 + "cm");
+        // 출력 : 5cm
+    }
+
+static class 사람 {
+    팔 a왼팔;
+
+    사람() {
+        a왼팔 = new 팔();
+    }
+}
+
+static class 팔 {
+    손 a손;
+
+    팔() {
+        a손 = new 손();
+    }
+}
+
+static class 손 {
+    손가락 a엄지손가락;
+
+    손() {
+        a엄지손가락 = new 손가락();
+        a엄지손가락.길이 = 5;
+    }
+}
+
+static class 손가락 {
+    int 길이;
+}
+}
