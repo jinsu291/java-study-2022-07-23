183428317d10d47bde473e13da7cbc37db6205e2
jinsu291 <wlstn291@naver.com>
2022-07-24 00:54:14 +0900

    Main14_6 추가

---
+++ src/main/java/a14_생성자1/Main14_6.java
@@ -0,0 +1,44 @@
+package a14_생성자1;
+
+// 문제 : 아래와 같이 출력 되도록 해주세요.
+
+public class Main14_6 {
+    public static void main(String[] args) {
+        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
+
+        System.out.println(엄지손가락_길이 + "cm");
+        // 출력 : 5cm
+    }
+
+    static class 사람 {
+        팔 a왼팔;
+
+        사람() {
+            a왼팔 = new 팔();
+        }
+    }
+
+    static class 팔 {
+        손 a손;
+
+        팔() {
+            a손 = new 손();
+        }
+    }
+
+    static class 손 {
+        손가락 a엄지손가락;
+
+        손() {
+            a엄지손가락 = new 손가락();
+        }
+    }
+
+    static class 손가락 {
+        int 길이;
+
+        손가락() {
+            길이 = 5;
+        }
+    }
+}
