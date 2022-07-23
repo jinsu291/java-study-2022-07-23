5bfa3153fc125e890cd9fec41193b8a324b6c98e
jinsu291 <wlstn291@naver.com>
2022-07-23 23:56:15 +0900

    Main13_2 추가

---
+++ src/main/java/a13_구성3/Main13_2.java
@@ -0,0 +1,58 @@
+package a13_구성3;
+
+// 힌트 : if ( a무기 == null ) { ... }
+
+public class Main13_2 {
+    public static void main(String[] args) {
+        전사 a전사 = new 전사();
+
+        a전사.a무기1 = new 칼();
+        a전사.공격();
+        // 출력 => 전사가 칼로 공격합니다.
+
+        a전사.a무기1 = new 활();
+        a전사.공격();
+        // 출력 => 전사가 활로 공격합니다.
+
+        a전사.a무기2 = new 칼();
+        a전사.공격();
+        // 출력 => 전사가 활로 공격합니다.
+        // 출력 => 전사가 칼로 공격합니다.
+    }
+
+    static class 전사 {
+        int 나이;
+        무기 a무기1;
+        무기 a무기2;
+
+        void 공격() {
+            if (a무기1 != null) {
+                System.out.print("전사가 ");
+                a무기1.작동();
+            }
+
+            if (a무기2 != null) {
+                System.out.print("전사가 ");
+                a무기2.작동();
+            }
+        }
+    }
+
+    static class 무기 {
+        void 작동() {
+        }
+    }
+
+    static class 칼 extends 무기 {
+        void 작동() {
+            System.out.println("칼로 공격 합니다.");
+        }
+    }
+
+    static class 활 extends 무기 {
+        void 작동() {
+            System.out.println("활로 공격 합니다.");
+        }
+    }
+}
+
