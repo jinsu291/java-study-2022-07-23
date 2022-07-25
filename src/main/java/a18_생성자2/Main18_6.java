c5b9dc6b412b4fee49e181e8d31c6ed18e4fe088
jinsu291 <wlstn291@naver.com>
2022-07-25 21:12:15 +0900

    Main18_6 추가

---
+++ src/main/java/a18_생성자2/Main18_6.java
@@ -0,0 +1,27 @@
+package a18_생성자2;
+
+// 문제 : 아래와 같이 출력 되도록 해보세요.[정답]
+public class Main18_6 {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+        int a사람의_왼팔_길이 = a사람.a왼팔.길이;
+        System.out.println(a사람의_왼팔_길이 + "cm");
+        // 출력 : 70cm
+    }
+
+    static class 사람 {
+        팔 a왼팔;
+
+        사람() {
+            this.a왼팔 = new 팔();
+        }
+    }
+
+    static class 팔 {
+        int 길이;
+
+        팔() {
+            길이 = 70;
+        }
+    }
+}
