7f908b61dde4843d22cf37bef75dc04936079b29
jinsu291 <wlstn291@naver.com>
2022-07-26 23:50:40 +0900

    Main24_9 추가

---
+++ src/main/java/a24_static/Main24_9.java
@@ -0,0 +1,14 @@
+package a24_static;
+
+// 문제 : 아래 코드의 잘 못된 점을 고쳐보세요.[정답]
+
+public class Main24_9 {
+    public static void main(String[] args) {
+        출력("안녕");
+        // 출력 : 안녕
+    }
+
+    static void 출력(String str) {
+        System.out.println(str);
+    }
+}
