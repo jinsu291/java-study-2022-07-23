aa25b7b9f2f9eb75dbc7de334b6a11b187f24d74
jinsu291 <wlstn291@naver.com>
2022-07-29 21:32:50 +0900

    Explanation28_1 ~ 28_2추가

---
+++ src/main/java/a28_ObjectClass/Explanation28_2.java
@@ -0,0 +1,45 @@
+package a28_ObjectClass;
+
+public class Explanation28_2 {
+    public static void main(String[] args) {
+        사람 a사람1 = new 사람("홍길동", 22);
+        사람 a사람2 = new 사람("홍길동", 22);
+
+        if (a사람1.equals("ㅋㅋ")) {
+            System.out.println("거짓1");
+        }
+
+        if (a사람1.equals(a사람2)) {
+            System.out.println("참1");
+        }
+    }
+
+    static class 사람 {
+        String 이름;
+        int 나이;
+
+        사람(String 이름, int 나이) {
+            this.이름 = 이름;
+            this.나이 = 나이;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof 사람 == false) {
+                return false;
+            }
+
+            사람 other = (사람) o;
+
+            if (!이름.equals(other.이름)) {
+                return false;
+            }
+
+            if (this.나이 != other.나이) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
